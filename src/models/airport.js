b3372051222893c01813014b0cfc116126d56db6
Prateek-5 <prateek.ku11@gmail.com>
2024-01-02 15:23:56 +0530

    added the seeders file for airport

--- src/models/airport.js
+++ src/models/airport.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsTo(Model.City,{
+      this.belongsTo(models.City,{
         foreignKey:'cityId',
         onDelete:'CASCADE',
       })
